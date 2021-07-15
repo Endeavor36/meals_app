1c586724c29da9c71f71121ab153346ef7852c36
Endeavor36 <allmight524@gmail.com>
2021-07-15 21:17:42 +0530

    init

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String?, String?>;
+
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+
+    return Center(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle!),
+        ),
+        body: Center(
+          child: Text(
+            'Recipes for selected category',
+          ),
+        ),
+      ),
+    );
+  }
+}
